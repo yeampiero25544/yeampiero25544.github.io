9da1f49b7101f379acfc4d62ec79261ca1d1b6d7
yeampiero25544 <53464356436436gg@gmail.com>
2024-07-17 14:21:23 -0400

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,8 @@
+// Ads terra Historia
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '//noisesperusemotel.com/6c/9c/ca/6c9cca2344310fc5890c46b7c2e6c2ca.js';
+
+// Agregar el elemento <script> al final del body
+document.body.appendChild(script);
+
